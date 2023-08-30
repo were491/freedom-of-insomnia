ee4c5f10bd2ff220e75e10e2b1bc898d211cec0f
unilock <unilock@fennet.rentals>
2023-08-30 15:50:29 -0400

    Update to 1.20.1, minor reformatting, major mixins refactor
    CommandMixin -> ServerPlayerEntityMixin no longer extends PlayerEntity, which was unnecessary
    
    PhantomSpawnerMixin is now much simpler, using a single Inject that captures locals instead of two ModifyVariables and a unique field

--- src/main/java/com/rha1117/freedomofinsomnia/InsomniaCommand.java
+++ src/main/java/com/rha1117/freedomofinsomnia/InsomniaCommand.java
@@ -18,52 +18,51 @@ public class InsomniaCommand implements ModInitializer {
 			LiteralCommandNode<ServerCommandSource> insomniaNode = CommandManager
 					.literal("insomnia")
 				        .build();
-			
+
 			LiteralCommandNode<ServerCommandSource> enableNode = CommandManager
 					.literal("enable")
 				        .executes(this::enableInsomnia)
 				        .build();
-			
+
 			LiteralCommandNode<ServerCommandSource> disableNode = CommandManager
 					.literal("disable")
 				        .executes(this::disableInsomnia)
 				        .build();
-			
+
 			LiteralCommandNode<ServerCommandSource> statusNode = CommandManager
 					.literal("status")
 				        .executes(this::displayStatus)
 				        .build();
-			
+
 			dispatcher.getRoot().addChild(insomniaNode);
 			insomniaNode.addChild(enableNode);
 			insomniaNode.addChild(disableNode);
 			insomniaNode.addChild(statusNode);
 		});
-		
+
 		// Register an event to persist insomnia status past death.
 		ServerPlayerEvents.COPY_FROM.register((oldPlayer, newPlayer, alive) ->
-				((CommandMixinInterface) newPlayer).setInsomniaDisabled( ((CommandMixinInterface) oldPlayer).getInsomniaDisabled() ));
+				((CommandMixinInterface) newPlayer).freedom_of_insomnia$setInsomniaDisabled( ((CommandMixinInterface) oldPlayer).freedom_of_insomnia$getInsomniaDisabled() ));
 	}
 
 	private int displayStatus(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-		if (((CommandMixinInterface) context.getSource().getPlayerOrThrow()).getInsomniaDisabled()) {
-			context.getSource().sendFeedback(Text.literal("Insomnia is currently disabled.").formatted(Formatting.RED), false);
-			return 0;
+		if (((CommandMixinInterface) context.getSource().getPlayerOrThrow()).freedom_of_insomnia$getInsomniaDisabled()) {
+			context.getSource().sendFeedback(() -> Text.literal("Insomnia is currently disabled.").formatted(Formatting.RED), false);
 		} else {
-			context.getSource().sendFeedback(Text.literal("Insomnia is currently enabled.").formatted(Formatting.GREEN), false);
-			return 1;
+			context.getSource().sendFeedback(() -> Text.literal("Insomnia is currently enabled.").formatted(Formatting.GREEN), false);
 		}
+		return 0;
 	}
-	
+
 	private int enableInsomnia(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-		((CommandMixinInterface) context.getSource().getPlayerOrThrow()).setInsomniaDisabled(false);
-		context.getSource().sendFeedback(Text.literal("Insomnia is now enabled.").formatted(Formatting.GREEN), false);
-		return 1;
+		((CommandMixinInterface) context.getSource().getPlayerOrThrow()).freedom_of_insomnia$setInsomniaDisabled(false);
+		context.getSource().sendFeedback(() -> Text.literal("Insomnia is now enabled.").formatted(Formatting.GREEN), false);
+		return 0;
 	}
-	
+
 	private int disableInsomnia(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-		((CommandMixinInterface) context.getSource().getPlayerOrThrow()).setInsomniaDisabled(true);
-		context.getSource().sendFeedback(Text.literal("Insomnia is now disabled.").formatted(Formatting.RED), false);
-		return 1;
+		((CommandMixinInterface) context.getSource().getPlayerOrThrow()).freedom_of_insomnia$setInsomniaDisabled(true);
+		context.getSource().sendFeedback(() -> Text.literal("Insomnia is now disabled.").formatted(Formatting.RED), false);
+		return 0;
 	}
 }
--- src/main/java/com/rha1117/freedomofinsomnia/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/rha1117/freedomofinsomnia/mixin/ServerPlayerEntityMixin.java
@@ -1,42 +1,34 @@
 package com.rha1117.freedomofinsomnia.mixin;
 
-import net.minecraft.network.encryption.PlayerPublicKey;
-import org.jetbrains.annotations.Nullable;
+import com.rha1117.freedomofinsomnia.CommandMixinInterface;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.authlib.GameProfile;
-import com.rha1117.freedomofinsomnia.CommandMixinInterface;
-
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
 @Mixin(ServerPlayerEntity.class)
-public abstract class CommandMixin extends PlayerEntity implements CommandMixinInterface {
-	public CommandMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile, @Nullable PlayerPublicKey publicKey) {
-		super(world, pos, yaw, gameProfile, publicKey);
-	}
-	
+public abstract class ServerPlayerEntityMixin implements CommandMixinInterface {
+	@Unique
 	private boolean insomniaDisabled;
 
-	public void setInsomniaDisabled(boolean disabled) {
+	@Override
+	public void freedom_of_insomnia$setInsomniaDisabled(boolean disabled) {
 		insomniaDisabled = disabled;
 	}
-	
-	public boolean getInsomniaDisabled() {
+
+	@Override
+	public boolean freedom_of_insomnia$getInsomniaDisabled() {
 		return insomniaDisabled;
 	}
-	
+
 	@Inject(method = "writeCustomDataToNbt", at = @At("RETURN"))
 	public void writeCustomDataToNbt (NbtCompound tag, CallbackInfo info) {
 		tag.putBoolean("insomniaDisabled", insomniaDisabled);
 	}
-	
+
 	@Inject(method = "readCustomDataFromNbt", at = @At("RETURN"))
 	public void readCustomDataFromNbt(NbtCompound tag, CallbackInfo info) {
 		// Returns false if the tag doesn't already exist.
